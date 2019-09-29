df3a984064b59ca2c33dcfdab28a71c5645253eb
zhengchaobin <zhengchaobin@yunnex.com>
2019-09-29 11:55:42 +0800

    update custom error

--- CustomError.go
+++ CustomError.go
@@ -8,6 +8,20 @@ func(customError CustomError) Error() string  {
 	return customError.msg
 }
 
+//函数定义返回类型为指针类型时， if e != nil 会为true, 此时调用Error函数时会报空指针异常
+//详见example2.ErrorTest.customErrorTest2
+//func(customError *CustomError) Error() string  {
+//	if  customError == nil {
+//		return ""
+//	}
+//	return customError.msg
+//}
+
+
 func New(msgText string) CustomError {
 	return CustomError{msg: msgText}
+}
+
+func New2(msgText string) *CustomError {
+	return &CustomError{msg: msgText}
 }
